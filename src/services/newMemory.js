7ee468b22d209a83d874a439229951a5cb199d0f
EsotericZ <cjsand03@gmail.com>
2023-06-01 13:30:51 -0600

    Updates

--- src/services/newMemory.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-const newMemory = async (newMem) => {
-    const res = await api.post('/memories/newMemory', newMem);
-    return res.data;
-}
-
-export default newMemory;
